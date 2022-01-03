2bfcaba459e8caa744786f51010343d4af660b86
JessicaLinh <jessica.linh.dillingham@gmail.com>
2022-01-03 17:49:20 +0000

    .

---
+++ practice.py
@@ -0,0 +1,10 @@
+var1=input("enter a string: ")
+
+var2=len(var1)
+
+var3=var1.count("s")
+
+var4=var1.split()
+
+print(var4[len(var4)-1])
+
--- strings.py
+++
@@ -1,9 +0,0 @@
-
-var1=input("Gimme some text: ")
-
-var2=var1.find("world")
-
-print (var2)
-
-if var2 < 0:
-    print ("not found")
