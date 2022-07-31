d9600aa00f036d42de5bb0c1466821b796305671
jessica <jessica@Downstairs-Mac-mini.local>
2022-07-31 16:16:47 +0100

    .

--- findstheaverage.py
+++ findstheaverage.py
@@ -1,25 +1,27 @@
 
-
-def Average(my_list):
-    avg = sum(1) / len(1)
-    return avg
-
 my_total = 0  
 my_avg = 0  
+my_count = 0
 
-for i in range(5):
+while True:
 
     my_num = int(input("enter a digit between 0 and 10: "))
 
+    if my_num == -1:
+        print("Goodbye :D")
+        break
+
     if my_num > 10 or my_num < 0:
         print("Your number must be between 0 and 10 inclusive")
-        quit()
+        break
     
     my_total = my_total + my_num
+    my_count += 1
 
-#print("the total is " + str(my_total))
+print("the total is " + str(my_total))
 
-my_avg = my_total / 5
+if my_count != 0: 
+    my_avg = my_total / my_count
 
 print("the average is " + str(my_avg))
 
